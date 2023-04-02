8e0954481a96f75fae412656a941e2cc38508047
MariaAfteni <maria.afteni@isa.utm.md>
2023-04-02 15:16:16 +0300

    Added Lab 2

--- LFAF Lab/src/Transition.java
+++
@@ -1,9 +0,0 @@
-public record Transition(char currentState, char transitionLabel, char nextState) {
-
-    @Override
-    public String toString() {
-        return "currentState = " + currentState + "\n" +
-                "transitionLabel = " + transitionLabel + "\n" +
-                "nextState = " + nextState + "\n";
-    }
-}
